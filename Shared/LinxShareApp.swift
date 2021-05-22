01df4ab41284c0434d4503f177cf751efd0e0287
Tim Simon <tim@mirahi.io>
2021-05-22 10:49:33 +0200

    Initial Commit

---
+++ Shared/LinxShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinxShareApp.swift
+//  Shared
+//
+//  Created by Tim Simon on 22/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct LinxShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
